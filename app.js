59de66515636395740e34235db094ac5fc603fd0
Vadim Kireev <tudomundoze@yandex.ru>
2022-05-27 21:51:01 +0300

    last error done

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.get('*', (req, res) => res.status(404).send({ message: 'Запрашиваемая страница не найдена' }));
+app.patch('*', (req, res) => res.status(404).send({ message: 'Запрашиваемая страница не найдена' }));
 
 app.listen(PORT);
